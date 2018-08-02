64dcc01dfbfb3419400a309c029f731e815c8d37
Bjaro <42039292+Bjaro@users.noreply.github.com>
2018-08-02 16:56:30 +0200

    Add files via upload

---
+++ Bjaro/js/rasinfo.js
@@ -0,0 +1,50 @@
+function myFunction() {
+    var x = document.getElementById("option1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("klickmeny1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction() {
+    var x = document.getElementById("klickmeny2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction() {
+    var x = document.getElementById("klickmeny3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction() {
+    var x = document.getElementById("klickmeny4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("klickmeny5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
